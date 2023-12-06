b3cbc555e4aa77b6c6bf9dfe042aec20ca683bef
Idera Nimah <iderayusuf9@gmail.com>
2023-12-06 15:52:40 +0100

    functionality next

---
+++ script.js
@@ -0,0 +1,6 @@
+const prevbtn = document.querySelector("#prevbtn");
+const nextbtn = document.querySelector("#nextbtn");
+const book = document.querySelector("#book");
+
+const paperOne = document.querySelector("#p_one");
+const paperTwo = document.querySelector("#p_two");
